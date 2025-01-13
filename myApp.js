109945a6f920829c2944da1afb36dd06fca4e4d3
StenSchmidt <schmidtsten1324@gmail.com>
2025-01-13 13:23:38 -0600

    fixed the bug

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 
 
-app.get("http://localhost:3000/", function(req, res) {
+app.get("/", function(req, res) {
     res.send("Hello Express");
 });
 
