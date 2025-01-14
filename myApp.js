5d3dafdb5180befcb7469de934188b003330ad62
StenSchmidt <schmidtsten1324@gmail.com>
2025-01-14 11:38:45 -0600

    i wrote app.use(express.static())

--- myApp.js
+++ myApp.js
@@ -5,6 +5,7 @@ let app = express();
 app.get("/", function(req, res) {
     let absolutePath = __dirname + '/views/index.html';
     res.sendFile(absolutePath);
+    app.use("/public", express.static(__dirname + "/public"));
 });
 
 
