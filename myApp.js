e56617d3598f3f8dcf6512423cddf25477722797
StenSchmidt <schmidtsten1324@gmail.com>
2025-01-14 11:40:57 -0600

    commented out old code

--- myApp.js
+++ myApp.js
@@ -3,8 +3,8 @@ let app = express();
 
 
 app.get("/", function(req, res) {
-    let absolutePath = __dirname + '/views/index.html';
-    res.sendFile(absolutePath);
+    // let absolutePath = __dirname + '/views/index.html';
+    // res.sendFile(absolutePath);
     app.use("/public", express.static(__dirname + "/public"));
 });
 
