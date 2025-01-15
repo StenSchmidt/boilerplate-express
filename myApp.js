9e18906c56f7b7ecb18e47375ad6ed5ffb9865cc
StenSchmidt <schmidtsten1324@gmail.com>
2025-01-15 15:44:07 -0600

    respond with hello json in json format at /json directory

--- myApp.js
+++ myApp.js
@@ -6,9 +6,11 @@ let app = express();
     // let absolutePath = __dirname + '/views/index.html';
     // res.sendFile(absolutePath);
 // });
-app.use("/public", express.static(__dirname + "/public"));
+//app.use("/public", express.static(__dirname + "/public"));
 
-
+app.get("/json", function(req, res) {
+    res.json({"message": "Hello json"});
+});
 
 
 
