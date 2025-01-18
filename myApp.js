b2386d7c924af76158adf24865ccdf1bf885025b
StenSchmidt <schmidtsten1324@gmail.com>
2025-01-18 17:54:12 -0600

    chained together a middlware function with the final handler

--- myApp.js
+++ myApp.js
@@ -3,10 +3,17 @@ let express = require('express');
 let app = express();
 
 
-app.use(function(req, res, next) {
-    console.log(req.method + " " + req.path + " - " + req.ip);
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
     next();
-})
+}, function(req, res) {
+    res.send({time: req.time});
+});
+
+// app.use(function(req, res, next) {
+//     console.log(req.method + " " + req.path + " - " + req.ip);
+//     next();
+// })
 
 
 //app.get("/", function(req, res) {
@@ -22,33 +29,6 @@ app.use(function(req, res, next) {
 //         res.json({"message": "Hello json"});
 //     }
 // });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
