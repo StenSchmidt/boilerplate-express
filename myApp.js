f6ec48fe2e99b865af14422839b1d6edc35c133b
StenSchmidt <schmidtsten1324@gmail.com>
2025-01-18 18:03:29 -0600

    sent a response using route parameters

--- myApp.js
+++ myApp.js
@@ -2,13 +2,16 @@ require('dotenv').config()
 let express = require('express');
 let app = express();
 
-
-app.get('/now', function(req, res, next) {
-    req.time = new Date().toString();
-    next();
-}, function(req, res) {
-    res.send({time: req.time});
+app.get('/:word/echo', function(req, res) {
+    res.send({echo: req.params.word});
 });
+
+// app.get('/now', function(req, res, next) {
+//     req.time = new Date().toString();
+//     next();
+// }, function(req, res) {
+//     res.send({time: req.time});
+// });
 
 // app.use(function(req, res, next) {
 //     console.log(req.method + " " + req.path + " - " + req.ip);
