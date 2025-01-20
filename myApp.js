217dc0cd4dde693051100e6706e15a2a00644b89
StenSchmidt <schmidtsten1324@gmail.com>
2025-01-19 20:09:31 -0600

    used string interpolation

--- myApp.js
+++ myApp.js
@@ -7,7 +7,7 @@ app.get('/name', function(req,res) {
     let firstName = req.query.first;
     let lastName = req.quary.last;
     let fullName = (firstName + " " + lastName);
-    res.json({name: fullName});
+    res.json({name: `${firstName} ${lastName}`});
 });
 
 
